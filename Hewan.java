596f385346fad632ecf55645ddbf40a466cca462
Baharudin Yusuf aka Abib <115156354+4bib@users.noreply.github.com>
2023-11-11 18:38:54 +0700

    Add files via upload

---
+++ Hewan.java
@@ -0,0 +1,12 @@
+public class Hewan {
+	protected String jenisSuara;
+	
+	public Hewan(String jenisSuara) {
+		this.jenisSuara = jenisSuara;
+	}
+	
+	public void berbunyi() {
+		System.out.println("Hewan Bersuara: "+jenisSuara);
+	}
+}
+
---
+++ Nonfiksi.java
@@ -0,0 +1,5 @@
+public class Nonfiksi extends Buku {
+	public Nonfiksi(String judul) {
+		super(judul, "Non-Fiksi");
+	}
+}
